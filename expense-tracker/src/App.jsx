e48f399404670cfb2b142b95b2c6ef13ed2cdc4b
fhayat81 <fhayatkhan81>
2025-01-25 10:06:34 +0530

    navbar component added

--- expense-tracker/src/App.jsx
+++ expense-tracker/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   
 
   return (
     <>
-      <p className='text-green-900'>Hello World</p>
+      <Navbar/>
     </>
   )
 }
