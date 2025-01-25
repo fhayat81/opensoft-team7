7d449c33c75f31034e2920f33773173b977609ae
fhayat81 <fhayatkhan81>
2025-01-25 10:46:36 +0530

    added Navbar

--- expense-tracker/src/components/Navbar.jsx
+++ expense-tracker/src/components/Navbar.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlineCancel } from "react-icons/md";
 
 const Navbar = () => {
+    const [menu, setMenu] = useState(false);
   return (
-    <div>
-      
+    <div className='py-2 px-4 shadow-lg flex justify-between items-center'>
+      <div className='mx-2'>
+        <p className='font-extrabold text-3xl'>.EXPENSE TRACKER</p>
+      </div>
+      <div className='mx-2'>
+        <button className='text-3xl active:scale-90 transition-all duration-300' onClick={()=>{setMenu(!menu)}}>{(menu?<MdOutlineCancel />:<GiHamburgerMenu />)}</button>
+      </div>
     </div>
   );
 }
