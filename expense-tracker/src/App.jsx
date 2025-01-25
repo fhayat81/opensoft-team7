706341a536d37c4a6a666c0477bc3f84755a1357
fhayat81 <fhayatkhan81>
2025-01-25 11:30:48 +0530

    Added menu in Navbar

--- expense-tracker/src/App.jsx
+++ expense-tracker/src/App.jsx
@@ -1,9 +1,18 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 300, // Set default animation duration (optional)
+      easing: "ease-in-out", // Set default easing (optional)
+      once: true, // Whether animation should happen only once (optional)
+    });
+  }, []);
   
-
   return (
     <>
       <Navbar/>
--- expense-tracker/src/components/Navbar.jsx
+++ expense-tracker/src/components/Navbar.jsx
@@ -1,19 +1,57 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { MdOutlineCancel } from "react-icons/md";
 
 const Navbar = () => {
-    const [menu, setMenu] = useState(false);
+  const [menu, setMenu] = useState(false);
+  useEffect(() => {
+    const menuClose = () => {
+      setMenu(false);
+    }
+    window.addEventListener('click', menuClose);
+
+    return () => {
+      window.removeEventListener('click', menuClose);
+    };
+  }, []);
   return (
-    <div className='py-2 px-4 shadow-lg flex justify-between items-center'>
-      <div className='mx-2'>
-        <p className='font-extrabold text-3xl'>.EXPENSE TRACKER</p>
+    <div>
+      <div className="py-2 px-4 shadow-lg flex justify-between items-center">
+        <div className="mx-2">
+          <p className="font-extrabold text-3xl">.EXPENSE TRACKER</p>
+        </div>
+        <div className="mx-2">
+          <button
+            className="text-3xl active:scale-90 transition-all duration-300"
+            onClick={(e) => {
+              setMenu(!menu);
+              e.stopPropagation();
+            }}
+          >
+            {menu ? <MdOutlineCancel /> : <GiHamburgerMenu />}
+          </button>
+        </div>
       </div>
-      <div className='mx-2'>
-        <button className='text-3xl active:scale-90 transition-all duration-300' onClick={()=>{setMenu(!menu)}}>{(menu?<MdOutlineCancel />:<GiHamburgerMenu />)}</button>
+      <div>
+        <div
+          className={`absolute ${
+            menu ? "" : "translate-x-[200px]"
+          } transition-all duration-300 right-0 w-[200px] bg-white shadow-2xl p-2 h-full flex flex-col space-y-1`}
+        >
+          <p className="px-2 hover:scale-105 active:scale-95 cursor-pointer py-1 hover:bg-black hover:text-white rounded-sm transition-all duration-300">Dashboard</p>
+          <hr />
+          <p className="px-2 hover:scale-105 active:scale-95 cursor-pointer py-1 hover:bg-black hover:text-white rounded-sm transition-all duration-300">Dashboard</p>
+          <hr />
+          <p className="px-2 hover:scale-105 active:scale-95 cursor-pointer py-1 hover:bg-black hover:text-white rounded-sm transition-all duration-300">Dashboard</p>
+          <hr />
+          <p className="px-2 hover:scale-105 active:scale-95 cursor-pointer py-1 hover:bg-black hover:text-white rounded-sm transition-all duration-300">Dashboard</p>
+          <hr />
+          <p className="px-2 hover:scale-105 active:scale-95 cursor-pointer py-1 hover:bg-black hover:text-white rounded-sm transition-all duration-300">Dashboard</p>
+          <hr />
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default Navbar;
